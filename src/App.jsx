e0262394ded9c829b78d067b39626f8e6afc92d2
meena-purohit <mpurohit7890@gmail.com>
2025-12-22 13:37:33 +0530

    add a button for count val

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Header from "./header";
 import User from "./User";
 function App(){
+  const [counter,setCounter]=useState(0)
   return(
     <div>
       <User />
       <Header />
     <h1>hello</h1>
+    <h2>counter val :{counter} </h2>
+    <button onClick={()=>setCounter(counter+1)}>Increse Counter Val</button>
     <h1>code step by step</h1>
     </div>
   );
