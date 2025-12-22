4009c67dfa8ffe9713164095d343cda3927823fb
meena-purohit <mpurohit7890@gmail.com>
2025-12-22 16:01:11 +0530

    learn difference between component and function, add alert example to understand functions in React

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import { useState } from "react";
 import Header from "./header";
 import User from "./User";
 function App(){
+  alert(sum());
   const [counter,setCounter]=useState(0)
   return(
     <div>
       <User />
       <Header />
-    <h1>hello</h1>
+      <Fruit />
+    <h1>hello{sum()}</h1>
     <h2>counter val :{counter} </h2>
     <button onClick={()=>setCounter(counter+1)}>Increse Counter Val</button>
     <h1>code step by step</h1>
     </div>
   );
 }
+function Fruit() {
+  return(
+    <h2>Apple</h2>
+  )
+}
 
+function sum() {
+  return(
+    10+10
+  )
+}
 export default App;
