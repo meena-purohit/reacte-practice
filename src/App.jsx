e5853f8ebfcc0db4d8a44970f268487e5f60c031
meena-purohit <mpurohit7890@gmail.com>
2025-12-22 13:20:14 +0530

    update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Header from "./header";
+import User from "./User";
 function App(){
   return(
     <div>
+      <User />
       <Header />
     <h1>hello</h1>
     <h1>code step by step</h1>
