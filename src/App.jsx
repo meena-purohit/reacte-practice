653b4645d6cb4f93e9765df8ef8109eb55fae25d
meena-purohit <mpurohit7890@gmail.com>
2025-12-31 14:07:03 +0530

    Practice toggle and multiple conditions in React state

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
  import { useState } from "react";
-import Counter from "./Counter";
-  
  function App() {
- const [fruit,setFruit]=useState("Apple");
- const handleFruit=()=> {
-  setFruit("Banana");
- }
+  const [count,setCount]=useState(0);
+
   return (
     <div>
-      <h1>State in React Js</h1>
-      <h1>{fruit}</h1>
-     <button onClick={handleFruit}>change fruit Name</button>
-     <Counter/>
+ <h1>{count}</h1>
+ <button onClick={()=>setCount(count+1)}>counter</button>
+{
+  count==0?<h1>Condition 0</h1>
+  :count==1?<h1>Condition 1</h1>
+  :count==2?<h1>Condition 2</h1>
+  :count==3?<h1>Condition 3</h1>
+  :count==4?<h1>Condition 4</h1>
+  : <h1>Other Condition</h1>
+}
     </div>
   )
  }
 
- export default App 
+ export default App;
