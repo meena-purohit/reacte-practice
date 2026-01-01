30ddd3ceac66c4b02d96286cbba189106fb73d83
meena-purohit <mpurohit7890@gmail.com>
2026-01-01 06:39:10 +0530

    Implement React props and data flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
- import { useState } from "react";
- function App() {
-  const [count,setCount]=useState(0);
+import College from "./College";
+import User from "./User";
 
-  return (
+ 
+ function App() {
+  // let userName = "Meena Purohit";
+  // let age = 29;
+  // let email = "meena@test.com";
+  let userObject1 = {
+     name:"Meena Purohit",
+     age:29,
+     email:"meena@test.com"
+
+  }
+let userObject2 = {
+     name:"Peater",
+     age:20,
+     email:"peater@test.com"
+
+  }
+  let userObject3 = {
+     name:"Peater",
+     age:20,
+     email:"peater@test.com"
+
+  }
+  let collegeNames = ["IET","DU","ITI","NIT","NET"]
+  return(
     <div>
- <h1>{count}</h1>
- <button onClick={()=>setCount(count+1)}>counter</button>
-{
-  count==0?<h1>Condition 0</h1>
-  :count==1?<h1>Condition 1</h1>
-  :count==2?<h1>Condition 2</h1>
-  :count==3?<h1>Condition 3</h1>
-  :count==4?<h1>Condition 4</h1>
-  : <h1>Other Condition</h1>
-}
+      <h1>Props in React Js</h1>
+      {/* <User name={userName} age={age} email={email}/> */}
+      <College name={collegeNames[0]}/>
+      <College name={collegeNames[1]}/>
+      <College name={collegeNames[2]}/>
+      <College name={collegeNames[3]}/>
+      <College name={collegeNames[4]}/>
+
+
+
+      <User user={userObject1}/>
+      <User user={userObject2}/>
+      <User user={userObject3}/>
     </div>
   )
  }
--- src/User.jsx
+++ src/User.jsx
@@ -1,7 +1,11 @@
-function User() {
+function User({user}) {
     return (
         <div>
-            <h1>User Component</h1>
+             <hr />
+            <h2>Name:{user.name}</h2>
+            <h3>Age:{user.age}</h3>
+            <h3>email:{user.email}</h3>
+           
         </div>
     )
 }
