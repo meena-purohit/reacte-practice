46f87900ad02ff704c6e52740334d75caa455224
meena-purohit <mpurohit7890@gmail.com>
2026-01-22 20:45:53 +0530

    Learned Dynamic and Conditional Inline Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,136 @@
-import myImage from "./assets/test-external-2519778_1920.jpg"
+import { useState } from "react";
+import myImage from "./assets/test-external-2519778_1920.jpg";
 function App() {
 
-  const cardStyle = {border:"2px solid #cccccc28",
-          width:"200px",
-          boxShadow: "1px 2px 3px 0px #cccccc57",
-          margin: "10px"}
-  return(
-    <div>
-      <h1 style={{color:"red"}}>Inline Style in React</h1>
-      <div style={{display:"flex",flexWrap:"wrap"}}>
-      <div style={cardStyle}>
-        <img style={{width:"200px"}} src={myImage} alt="not found"/>
-         <div>
-      <h4>Meena Purohit</h4>
-      <p>Software Developer</p>
-     </div>
-     </div>
-     <div style={cardStyle}>
-        <img style={{width:"200px"}} src={myImage} alt="not found"/>
-         <div>
-      <h4>Meena Purohit</h4>
-      <p>Software Developer</p>
-     </div>
-     </div>
+  const [cardStyle,setCardStyle] = useState( {
+    border: "2px solid #cccccc28",
+    width: "200px",
+    boxShadow: "1px 2px 3px 0px #cccccc57",
+    margin: "10px",
+  })
+  const [grid,setGrid] = useState(true)
+  const [textColor,setTextColor]= useState('green')
 
-     </div>
-     </div>
-    
-  )
+  const updateTheme=((bgColor,textColor)=>{
+    setCardStyle({...cardStyle,backgroundColor:bgColor})
+    setTextColor()
+  })
+  
+  return (
+    <>
+      <h2>Dynamic and Conditional Inline Style</h2>
+      <button onClick={()=>updateTheme('gray','green')}>Gray Theme</button>
+      <button onClick={()=>updateTheme('white','black')}>Default Theme</button>
+      <button onClick={()=>setGrid(!grid)}>Toggle Grid</button>
+      <div style={{ display: grid?"flex":"block", flexWrap:"wrap"}}>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+        <div style={cardStyle}>
+          <img style={{ width:"200px"}} src={myImage} alt="not found"/>
+          <div style={{padding:'5px',color:textColor}}>
+            <h4>Meena Purohit</h4>
+            <p>Software Developer</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
