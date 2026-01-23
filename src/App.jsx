fcda96682949b209a6798b0c9539f632cd7be67e
meena-purohit <mpurohit7890@gmail.com>
2026-01-23 09:54:06 +0530

    Learned Styled Component with React Js

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-import UserProfile from "./UserProfile";
-import "./css/style.css"
-
-
+import styled from "styled-components";
 
 function App() {
-  return (
-    <div>
-      <h1 className="heading">Style with CSS Module in React Js</h1>
-      <div style={{display:"flex",flexWrap:"wrap"}}>
-        <UserProfile/>
-        <UserProfile/>
-        <UserProfile/>
-        <UserProfile/>
-        <UserProfile/>
-        <UserProfile/>
-        <UserProfile/>
-        <UserProfile/>
-        <UserProfile/>
-        <UserProfile/>
-        <UserProfile/>
-        <UserProfile/>
-        <UserProfile/>
-        <UserProfile/>
+
+  const Heading = styled.h1`
+  color: red;
+  border: 1px solid green;
+  border-Radius: 5px;
+  margin: 20px;
+  padding: 20px;`
+
+  const Stylbtn = styled.button`
+  border: 2px solid black;
+  border-radius:5px;
+  height:40px;
+  width:130px;
+  margin:20px;`
+
+  return(
+      <div>
+        <h1>Styled Component with React Js</h1>
+        <Heading>Hello Heading</Heading>
+         <Heading>Hello Heading</Heading>
+          <Heading>Hello Heading</Heading>
+           <Heading>Hello Heading</Heading>
+            <Heading>Hello Heading</Heading>
+            <Stylbtn>signin</Stylbtn>
+            <Stylbtn>signup</Stylbtn>
+            <Stylbtn>exit</Stylbtn>
+            <Stylbtn>back</Stylbtn>
+
       </div>
-    </div>
-  );
+  )
 }
 
 export default App;
--- src/Notes.jsx
+++ src/Notes.jsx
@@ -637,3 +637,40 @@
 // }
 
 // export default UserProfile;
+
+// import styled from "styled-components";
+
+// function App() {
+//   const Heading = styled.h1`
+//     color: red;
+//     border: 1px solid green;
+//     border-radius: 5px;
+//     margin: 20px;
+//     padding: 20px;
+//   `;
+
+//   const Stylbtn = styled.button`
+//     border: 2px solid black;
+//     border-radius: 5px;
+//     height: 40px;
+//     width: 130px;
+//     margin: 20px;
+//   `;
+
+//   return (
+//     <div>
+//       <h1>Styled Component with React Js</h1>
+//       <Heading>Hello Heading</Heading>
+//       <Heading>Hello Heading</Heading>
+//       <Heading>Hello Heading</Heading>
+//       <Heading>Hello Heading</Heading>
+//       <Heading>Hello Heading</Heading>
+//       <Stylbtn>signin</Stylbtn>
+//       <Stylbtn>signup</Stylbtn>
+//       <Stylbtn>exit</Stylbtn>
+//       <Stylbtn>back</Stylbtn>
+//     </div>
+//   );
+// }
+
+// export default App;
