0d4bab0b4d5ce949e2a3ae4e2219df3e1b582086
meena-purohit <mpurohit7890@gmail.com>
2026-02-02 21:29:38 +0530

    Learned Updating Object in React js

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
 import { useState } from "react";
-import AddUser from "./AddUser";
-import DisplayUser from "./DisplayUser";
+
 
 function App() {
 
-    const[user,setUser] = useState('');
+  const [data, setData] = useState({
+    name:"Meena purohit",
+    address:{
+        city:"Delhi",
+        country:"India"
+    }
+  })
+  const handleName = (val)=>{
+    data.name=val
+
+    console.log(data);
+    setData({...data})
+}
+const handleCity=(city)=>{
+  data.address.city=city
+  setData({...data,address:{...data.address,city}})
+}
     return(
 
         <div>
-            {/* <h1>Lifting State Up React Js</h1> */}
-            <AddUser setUser={setUser}/>
-            <DisplayUser user={user}/>
-           
+            <h1>Updating Object in State React Js</h1>
+            
+            <input type="text" placeholder="update name" 
+            onChange={(event)=>handleName(event.target.value)}/>
+
+             <input type="text" placeholder="update city" 
+            onChange={(event)=>handleCity(event.target.value)}/>
+            <h2>Name:{data.name}</h2>
+            <h2>City:{data.address.city}</h2>
+            <h2>Country:{data.address.country}</h2>
         </div>
     )
 }
