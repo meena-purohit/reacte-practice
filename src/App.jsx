6413eacb01f55f07e806353955fcee5632a1e995
meena-purohit <mpurohit7890@gmail.com>
2026-02-04 16:06:15 +0530

    Learned Updating Array in State

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,45 @@
 import { useState } from "react";
 
-
 function App() {
-
-  const [data, setData] = useState({
-    name:"Meena purohit",
-    address:{
-        city:"Delhi",
-        country:"India"
-    }
-  })
-  const handleName = (val)=>{
-    data.name=val
-
+  const [data, setData] = useState(["sam", "peter", "bruce", "tony"]);
+  const [dataDetails, setDataDetails] = useState([
+    { name: "meena", age: "25" },
+    { name: "sam", age: "20" },
+    { name: "pater", age: "21" },
+  ]);
+  const handelUser = (name) => {
+    data[data.length - 1] = name;
     console.log(data);
-    setData({...data})
-}
-const handleCity=(city)=>{
-  data.address.city=city
-  setData({...data,address:{...data.address,city}})
-}
-    return(
+    setData([...data]);
+  };
+  const handleAge=(age)=>{
+   dataDetails[dataDetails.length-1].age=age
+   console.log(dataDetails)
+   setDataDetails([...dataDetails])
+   
+  }
+  return (
+    <div>
+      <h1>Updating Array in State</h1>
+      <input
+        type="text"
+        placeholder="Enter last user name"
+        onChange={(event) => handelUser(event.target.value)}
+      />
+      {data.map((item, index) => (
+        <h3 key={index}>{item}</h3>
+      ))}
 
-        <div>
-            <h1>Updating Object in State React Js</h1>
-            
-            <input type="text" placeholder="update name" 
-            onChange={(event)=>handleName(event.target.value)}/>
-
-             <input type="text" placeholder="update city" 
-            onChange={(event)=>handleCity(event.target.value)}/>
-            <h2>Name:{data.name}</h2>
-            <h2>City:{data.address.city}</h2>
-            <h2>Country:{data.address.country}</h2>
-        </div>
-    )
+      <hr />
+      <input type="text" placeholder="Enter last user age"
+      onChange={(event)=>handleAge(event.target.value)} />
+      {
+        dataDetails.map((item,index)=>(
+          <h4 key={index}>{item.name},{item.age}</h4>
+        ))
+      }
+    </div>
+  );
 }
 
 export default App;
