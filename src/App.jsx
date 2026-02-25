d4c9ddce4fa902c26483be3a555c6d3e75a30890
meena-purohit <mpurohit7890@gmail.com>
2026-02-25 22:18:25 +0530

    Learned Integrate JSON Server APIs and Display Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,43 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import "./App.css"
 
- export default function App(){
-  const [userData,setUserData]=useState([]);
-  useEffect(()=>{
- getUsersData();
-  },[])
-   async function getUsersData() {
-    const url = "https://dummyjson.com/users";
-    let response =await fetch(url);
-    response = await response.json()
-     setUserData(response.users); 
-  }
- 
-  console.log(userData);
-  
-  return(
+export default function App() {
+  const [userData, setUserData] = useState([]);
+  const [loading,setLoading]=useState(false);
+
+  useEffect(() => {
+    getUserData();
+    setLoading(true);
+  }, []);
+
+  const getUserData = async () => {
+    const url = "http://localhost:3000/users";
+    let response = await fetch(url);
+    response = await response.json();
+    setUserData(response);
+    setLoading(false);
+  };
+
+  return (
     <div>
-    <h1>Fetch data from API</h1>
-    <ul className="user-list-h">
-        <li>First Name</li>
-        <li>Last Name</li>
+      <h1>Intergrate JSON Server API and Loader</h1>
+      <ul className="user-list-h">
+        <li>Name</li>
         <li>Age</li>
-       </ul>
-    {
-     userData && userData.map((user)=>(
+        <li>Email</li>
+      </ul>
+      {
+      !loading?
+      userData.map((user) => (
        <ul className="user-list">
-        <li>{user.firstName}</li>
-        <li>{user.lastName}</li>
+        <li>{user.name}</li>
         <li>{user.age}</li>
+        <li>{user.email}</li>
        </ul>
       ))
+    :<h1>Data Loading...</h1>
     }
+      
     </div>
-  )
+  );
 }
