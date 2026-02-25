2081da9f260d0ff9eed3950ef5bf190fce300ffe
meena-purohit <mpurohit7890@gmail.com>
2026-02-25 17:59:57 +0530

    Learned Fetch data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react"
+import "./App.css"
+
  export default function App(){
+  const [userData,setUserData]=useState([]);
+  useEffect(()=>{
+ getUsersData();
+  },[])
+   async function getUsersData() {
+    const url = "https://dummyjson.com/users";
+    let response =await fetch(url);
+    response = await response.json()
+     setUserData(response.users); 
+  }
+ 
+  console.log(userData);
+  
   return(
     <div>
-     <h2 className="bg-gray-200 text-3xl border-green-700 border-8 ">Tailwind Setup</h2>
+    <h1>Fetch data from API</h1>
+    <ul className="user-list-h">
+        <li>First Name</li>
+        <li>Last Name</li>
+        <li>Age</li>
+       </ul>
+    {
+     userData && userData.map((user)=>(
+       <ul className="user-list">
+        <li>{user.firstName}</li>
+        <li>{user.lastName}</li>
+        <li>{user.age}</li>
+       </ul>
+      ))
+    }
     </div>
   )
 }
