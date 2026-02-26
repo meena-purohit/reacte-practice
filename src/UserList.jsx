98b40444c202622fcc43866a88adffcf66f0e837
meena-purohit <mpurohit7890@gmail.com>
2026-02-26 17:17:20 +0530

    Learned Integrate  API for DELETE Method

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 
 export default function UserList() {
   const [userData, setUserData] = useState([]);
-  const [loading,setLoading]=useState(false);
+  const [loading, setLoading] = useState(false);
+   const url = "http://localhost:3000/users";
 
   useEffect(() => {
     getUserData();
@@ -10,33 +11,46 @@ export default function UserList() {
   }, []);
 
   const getUserData = async () => {
-    const url = "http://localhost:3000/users";
-    let response = await fetch(url);
+  let response = await fetch(url);
     response = await response.json();
     setUserData(response);
     setLoading(false);
   };
 
+  const deleteUser =async(id)=>{
+    let response = await fetch(url+"/"+id,
+      {method:'delete'}
+    );
+    response = await response.json();
+    if( response){
+        alert("record deleted")
+    }
+    getUserData();
+
+  }
+
   return (
     <div>
-      
       <ul className="user-list-h">
         <li>Name</li>
         <li>Age</li>
         <li>Email</li>
+        <li>Action</li>
       </ul>
-      {
-      !loading?
-      userData.map((user) => (
-       <ul className="user-list">
-        <li>{user.name}</li>
-        <li>{user.age}</li>
-        <li>{user.email}</li>
-       </ul>
-      ))
-    :<h1>Data Loading...</h1>
-    }
-      
+      {!loading ? (
+        userData.map((user) => (
+          <ul className="user-list">
+            <li>{user.name}</li>
+            <li>{user.age}</li>
+            <li>{user.email}</li>
+            <li>
+              <button onClick={()=>deleteUser(user.id)}>Delete</button>
+            </li>
+          </ul>
+        ))
+      ) : (
+        <h1>Data Loading...</h1>
+      )}
     </div>
   );
 }
