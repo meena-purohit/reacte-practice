3140e0248f04689a6d7210365687fa763205fc41
meena-purohit <mpurohit7890@gmail.com>
2026-02-26 16:44:06 +0530

    Learned Integrate Post methode API

--- src/UserAdd.jsx
+++ src/UserAdd.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 function UserAdd(){
+    const [name,setName]=useState('');
+     const [age,setAge]=useState('');
+      const [email,setEmail]=useState('');
+
+      const createUser=async()=>{
+      console.log(name,age,email);
+      const url = "http://localhost:3000/users";
+      let response = await fetch(url,{
+        method:"post",
+        body:JSON.stringify({name,age,email})
+      });
+      response= await response.json();
+      if(response){
+        alert("new user added")
+      }
+      
+      }
     return(
-        <div>
-            <h2>User Add</h2>
+        <div style={{textAlign:"center"}}>
+            <h2> Add New User</h2>
+           
+             <input type="text"
+              onChange={(event)=>setName(event.target.value)} placeholder="Enter Name" />
+             <br /><br />
+             <input  type="text"
+             onChange={(event)=>setAge(event.target.value)} placeholder="Enter Age" />
+             <br /><br />
+             <input  type="text"
+             onChange={(event)=>setEmail(event.target.value)} placeholder="Enter Email" />
+             <br /><br />
+             <button onClick={createUser}>Add User</button>
+           
         </div>
     )
 }
