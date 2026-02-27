4e6e970fda195e5f62b57a05571053be6bb1ffe4
meena-purohit <mpurohit7890@gmail.com>
2026-02-27 11:12:43 +0530

    Learned simple validation in React js

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,44 @@
-
-import { NavLink, Route, Routes } from "react-router";
+import { useState } from "react";
 import "./App.css"
-import UserAdd from "./UserAdd";
-import UserList from "./UserList";
-import UserEdit from "./UserEdit";
 
 export default function App() {
+
+  const [name,setName]= useState('');
+  const [nameErr,setNameErr]= useState();
+
+  const [password,setPassword]= useState('');
+  const [passErr,setPassErr]= useState();
+
+  const handleName=(event)=>{
+    console.log(event.target.value);
+    if(event.target.value.length>5) {
+      setNameErr("Please enter valid username. 5 characters allowed")
+    } else{
+      setNameErr()
+    }
+    
+  }
+
+   const handlePassword=(event)=>{
+    let rejex = /^[A-Z0-9]+$/i;
+    if(rejex.test(event.target.value)) {
+       setPassErr()
+    } else{
+      setPassErr("Only Numbers and Alphabates allowed")
+    }
+    
+  }
  
 
   return (
     <div>
-      <ul className="nav-list">
-        <li>
-          <NavLink style={{textDecoration:"none"}} to="/">List</NavLink>
-        </li>
-        <li>
-          <NavLink style={{textDecoration:"none"}} to="/add">Add User</NavLink>
-        </li>
-      </ul>
-      {/* <h1>Make Routes and Pages for Add User and User List UI  </h1>
-      <UserList /> */}
-     
-     <Routes>
-      <Route path="/" element={<UserList />}/>
-      <Route path="/add" element={<UserAdd />}/>
-      <Route path="/edit/:id" element={<UserEdit />}/>
-     </Routes>
+     <input className={nameErr?"error":''} type="text" onChange={handleName} placeholder="enter name" />
+     <span className="red-color">{nameErr && nameErr}</span>
+     <br /><br />
+     <input  className={passErr?"error":''} type="text" onChange={handlePassword} placeholder="enter password" />
+     <span className="red-color">{passErr && passErr}</span>
+     <br /><br />
+     <button disabled={nameErr || passErr}>Login</button>
     </div>
   );
 }
