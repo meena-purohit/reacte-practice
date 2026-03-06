1e4f851d768db76b599c6953b61a6cc6ef47b054
meena-purohit <mpurohit7890@gmail.com>
2026-03-06 15:42:49 +0530

    Learned use API in React js

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import { lazy, Suspense, useState } from "react";
+import { Suspense, use } from "react";
 
-const User = lazy(()=>import('./Users'))
+const fetchData =()=> fetch('https://dummyjson.com/users').then((response)=>response.json());
+const userResource = fetchData();
+ 
+export default function App(){
+ 
 
-export default function App () {
-  
-  const [load,setLoad]=useState(false)
   return(
     <div>
-      <h1>Lazy Loading</h1>
-
-      <button onClick={()=>setLoad(true)}>Load User</button>
-
-      {
-      load?  <Suspense fallback={<h3>Loading...</h3>}><User /></Suspense> :null
-     } 
-     
+      <h1>use API in React js</h1>
+     <Suspense fallback={<p>Loading....</p>}>
+       <Users userResource={userResource} />
+     </Suspense>
     </div>
   )
+}
+
+const Users =({userResource})=>{
+  const userData = use(userResource);
+  console.log(userData.users);
+  
+  
+return(
+ <div>
+   <h1>Users List</h1>
+   {
+    userData?.users?.map((user)=>(
+      <div>
+        <h1>{user.firstName}   {user.lastName}</h1>
+     
+      </div>
+    ))
+   }
+ </div>
+)
 }
