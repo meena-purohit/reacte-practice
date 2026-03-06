ab37e9c37830e62caf3ac4165667eb9f8b87543d
meena-purohit <mpurohit7890@gmail.com>
2026-03-06 14:59:57 +0530

    Learned Lazy Loading in React Js

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,20 @@
-import { useReducer } from "react";
+import { lazy, Suspense, useState } from "react";
 
-const emptyData={
-  name:'',
-  passwoed:'',
-  email:'',
-  city:'',
-  address:''
-}
+const User = lazy(()=>import('./Users'))
 
-const reducer = (data,action)=>{
- return {...data,[action.type]:action.val}
-  
-}
 export default function App () {
-  const [state,dispatch]=useReducer(reducer,emptyData)
   
-  
+  const [load,setLoad]=useState(false)
   return(
     <div>
-      <h1>Use Reduser</h1>
-      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'name'})}
-       placeholder="enter name" />
-      <br /><br />
-      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'password'})} placeholder="enter password" />
-      <br /><br />
-      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'email'})} placeholder="enter email" />
-      <br /><br />
-      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'city'})} placeholder="enter city" />
-      <br /><br />
-      <input type="text" onChange={(event)=>dispatch({val:event.target.value,type:'address'})} placeholder="enter address" />
-      <br /><br />
-     <button onClick={()=>console.log(state)
-     }>Add Details</button>
-     <ul>
-      <li><h4>Name:{state.name}</h4></li>
-       <li><h4>Password:{state.password}</h4></li>
-        <li><h4>Email:{state.email}</h4></li>
-         <li><h4>City:{state.city}</h4></li>
-          <li><h4>Address:{state.address}</h4></li>
-     </ul>
+      <h1>Lazy Loading</h1>
+
+      <button onClick={()=>setLoad(true)}>Load User</button>
+
+      {
+      load?  <Suspense fallback={<h3>Loading...</h3>}><User /></Suspense> :null
+     } 
+     
     </div>
   )
 }
